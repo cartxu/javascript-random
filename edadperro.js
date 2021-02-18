e647d87abb270b4b8cbda3f6a7b0a7c4c198266f
cartxu <carmen.ns@gmail.com>
2021-02-18 20:38:19 +0100

    javascript play

--- edadperro.js
+++ edadperro.js
@@ -10,28 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (edadPerruna === 1) {
             
-            let respuestaEdad = document.createElement('p');
+            let respuestaEdad = document.querySelector('#respuesta');
             let edadHumana = 15;
-            respuestaEdad.innerHTML = `Con ${edadPerruna} año, la edad de tu perrete es de ${edadHumana} añazos humanos ¡feliz pubertad!`;
+            respuestaEdad.innerHTML = `Con ${edadPerruna} año, la edad de tu perrete es de ${edadHumana} añazos humanos ¡feliz pubertad! <br>
+            <img src="https://i1.wp.com/revista.weepec.com/wp-content/uploads/2017/01/perro-travieso.jpg">`;
             document.querySelector('body').append(respuestaEdad);
 
         } else if (edadPerruna === 2) {
 
-            let respuestaEdad = document.createElement('p');
+            let respuestaEdad = document.querySelector('#respuesta');
             let edadHumana = 15 + 9;
             respuestaEdad.innerHTML = `Con ${edadPerruna} años, la edad de tu perrete es de ${edadHumana} añazos humanos`;
             document.querySelector('body').append(respuestaEdad);
 
         } else if (edadPerruna > 2 && 1 !== 0) {
 
-            let respuestaEdad = document.createElement('p');
+            let respuestaEdad = document.querySelector('#respuesta');
             let edadHumana = ((edadPerruna - 2) * 5) + 15 + 9;
             respuestaEdad.innerHTML = `Con ${edadPerruna} años, la edad de tu perrete es de ${edadHumana} añazos humanos`;
             document.querySelector('body').append(respuestaEdad);
         } else {
 
-            let respuestaEdad = document.createElement('p');
-            respuestaEdad.innerHTML = `Si tu perro tiene 0 añicos, tranqui, sigue siendo un bebé.`;
+            let respuestaEdad = document.querySelector('#respuesta');
+            respuestaEdad.innerHTML = `Si tu perro tiene 0 añicos, tranqui, sigue siendo un bebé.
+            <img src="https://i.pinimg.com/originals/d7/a8/6e/d7a86e23a3c41f0423f8783c30c42657.jpg">`;
             document.querySelector('body').append(respuestaEdad);
         }
 
